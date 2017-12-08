945bbba780bcfee9cee1d8202d6c2db7028154fd
hengyuan-hu <kevin.huhy@gmail.com>
2017-12-07 19:47:22 -0500

    kfac trained.

--- cifar10_main.py
+++ cifar10_main.py
@@ -34,6 +34,30 @@ class Cifar10Model1(nn.Module):
         return y
 
 
+class Cifar10Model2(nn.Module):
+    def __init__(self):
+        super(Cifar10Model2, self).__init__()
+
+        model = module.Sequential()
+        model.add_module('c1', nn.Conv2d(3, 32, 3, 2, 1))
+        model.add_module('relu1', nn.ReLU())
+        model.add_module('c2', nn.Conv2d(32, 64, 3, 2, 1))
+        model.add_module('relu2', nn.ReLU())
+        model.add_module('l3', nn.Conv2d(64, 128, 3, 2, 1))
+        model.add_module('relu3', nn.ReLU())
+        model.add_module('avg_pool', nn.AvgPool2d(4))
+        self.main = model
+        self.fc = nn.Linear(128, 10)
+
+    def get_param_g(self):
+        return self.main.get_param_g() + [self.fc.get_param_g()]
+
+    def forward(self, x):
+        y = self.main(x).squeeze()
+        y = self.fc(y)
+        return y
+
+
 def set_all_seeds(rand_seed):
     def large_randint():
         return random.randint(int(1e5), int(1e6))
@@ -54,26 +78,27 @@ if __name__ == '__main__':
     print dataset.train_xs.min(), dataset.train_xs.max()
     print dataset.train_xs.shape
 
-    model = Cifar10Model1().cuda()
-    print model
-    t = time.time()
-    newton_loss, newton_acc = train(model, dataset, newton_grad, 0.1)
-    print 'time:', time.time() - t
+    # model = Cifar10Model1().cuda()
+    # print model
+    # t = time.time()
+    # newton_loss, newton_acc = train(model, dataset, newton_grad, 0.1)
+    # print 'time:', time.time() - t
 
-    model = Cifar10Model1().cuda()
+    # model = Cifar10Model1().cuda()
+    model = Cifar10Model2().cuda()
     t = time.time()
     normal_loss, normal_acc = train(model, dataset, normal_grad, 0.1)
     print 'time:', time.time() - t
 
     x = range(len(newton_loss))
     plt.figure()
-    plt.plot(x, newton_loss, 'r-', label='Newton Loss')
+    # plt.plot(x, newton_loss, 'r-', label='Newton Loss')
     plt.plot(x, normal_loss, 'b-', label='Normal Loss')
     plt.legend(loc='upper right')
     plt.savefig('cifar10_train_loss.png')
 
     plt.figure()
-    plt.plot(x, newton_acc, 'r-', label='newton Acc')#  t, t**3, 'g^')
+    # plt.plot(x, newton_acc, 'r-', label='newton Acc')#  t, t**3, 'g^')
     plt.plot(x, normal_acc, 'b-', label='normal Acc')#  t, t**3, 'g^')
     plt.legend(loc='lower right')
     plt.savefig('cifar10_test_acc.png')
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import numpy as np
 import module
 from dataset_wrapper import MnistWrapper
 import module
+from kfac import KFACOptimizer
 
 
 def normal_grad(loss, model):
@@ -13,37 +14,20 @@ def normal_grad(loss, model):
 def newton_grad(loss, model):
     loss.backward(retain_graph=True)
     gs = model.get_param_g()
-    # print 'gs:', gs
     grad = torch.zeros(len(gs))
-    # print gs[0].grad.data[0]
     for i, g in enumerate(gs):
         grad[i] = g.grad.data[0]
     hessian = torch.zeros(len(gs), len(gs))
     # compute second order grad
     dl_dgs = torch.autograd.grad(loss, gs, create_graph=True)
-    # ddl_ddg = torch.autograd.grad(dl_dgs, gs)
-    # print '>>>>>>>>>>>>>>', ddl_ddg
     for i, dl_dg in enumerate(dl_dgs):
         ddg = torch.autograd.grad(dl_dg, gs, retain_graph=True)
-        # print 'ddg', ddg
         hessian[i][i] = ddg[i].data[0]
-        # for j in range(len(ddg)):
-        #     hessian[i][j] = ddg[j].data[0]
-    # print hessian
+
     invh_grad = torch.mv(torch.inverse(hessian), grad)
     for i, g in enumerate(gs):
         g.grad.data[0] = invh_grad[i]
     nn.utils.clip_grad_norm(gs, 1)
-
-    # print gs[0].grad.data[0]
-    # print '--------'
-
-    # print hessian, grad, invh_grad
-    # print ddl_ddg.data[0]
-    # print g.grad.data[0]
-    # g.grad.data /= ddl_ddg.data
-    # print g.grad.data[0]
-    # dl_dg.backward()
 
 
 def train(model, dataset, grad_func, lr):
@@ -51,6 +35,8 @@ def train(model, dataset, grad_func, lr):
     num_epochs = 20
     # optim = torch.optim.RMSprop(model.parameters(), lr)
     optim = torch.optim.SGD(model.parameters(), lr)
+    # optim = KFACOptimizer(model)
+    optim.acc_stats = True
 
     train_loss = np.zeros(num_epochs)
     test_acc = np.zeros(num_epochs)
@@ -70,13 +56,12 @@ def train(model, dataset, grad_func, lr):
             grad_func(loss, model)
             optim.step()
             optim.zero_grad()
-            # break
 
-        # break
         train_loss[epoch] = losses.mean()
         test_acc[epoch] = eval_(model, dataset.test_xs, dataset.test_ys)
         print 'epoch: %i, loss: %.4f' % (epoch+1, train_loss[epoch])
         print 'eval acc:', test_acc[epoch]
+
     return train_loss, test_acc
 
 
